3a54a25ed0e4d06b3134a85a200705a41097dd25
AbubakarAbdulsalam <abakarsalam@gmail.com>
2020-04-08 19:56:41 -0400

    Setup Travis CI

--- src/test/java/Entities/ProductTest.java
+++ src/test/java/Entities/ProductTest.java
@@ -1,11 +1,35 @@
 package Entities;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProductTest {
+    private Product testProduct;
 
+    @Before
+    public void setUp()
+    {
+        testProduct = new Product();
+        testProduct.setProductName("Soap");
+
+    }
+
+
+    @Test
+    public void getProductId() {
+        assertEquals(testProduct.getProductId(), 0);
+    }
+
+    @Test
+    public void getProductName() {
+        assertTrue(testProduct.getProductName().equals("Soap"));
+    }
+
+    @Test
+    public void setProductName() {
+        testProduct.setProductName("New Name");
+        assertTrue(testProduct.getProductName().equals("New Name"));
+    }
 }
